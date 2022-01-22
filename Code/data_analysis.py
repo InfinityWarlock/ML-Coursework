3e731df384b14c50faea047a21e5a07d258f323e
InfinityWarlock <theo.hintonhallows123@gmail.com>
2022-01-22 10:17:58 +0000

    going to durham

--- Code/data_analysis.py
+++ Code/data_analysis.py
@@ -6,7 +6,6 @@ with open('data.csv', newline='') as file:
     reader = csv.reader(file, delimiter=',')
     keys = next(reader)
     data = np.loadtxt(file, delimiter=',')
-print(data.shape)
 
 # for i in data:
 #     print(i.shape)
@@ -21,7 +20,6 @@ def gender_box_plot(set_index, set_name):
                     split_sets[gender-1].append(i[set_index])
             else:
                 split_sets[gender-1].append(i[set_index])
-    print(split_sets)
     fig, ax = plt.subplots()
     ax.set_title(set_name)
     ax.boxplot(split_sets)
@@ -36,7 +34,6 @@ def by_age(set_index, set_name):
         if age < 150:
             filtered_data[0].append(age)
             filtered_data[1].append(i[set_index])
-    print(filtered_data)
     ax.plot(filtered_data[0], filtered_data[1], 'ro')
     plt.show()
 
@@ -44,11 +41,12 @@ def age_bplot():
     fdata = []
     for i in data:
         age = i[36]
-        if age < 150:
+        if age < 124:
             fdata.append(age)
     fig, ax = plt.subplots()
-    ax.set_title('Age distribution')
-    ax.boxplot(fdata)
+    ax.set_title('Age distribution in humour styles data')
+    ax.boxplot(fdata, showmeans=True)
+    ax.grid(axis='y', linestyle = '--')
     plt.show()
 
 age_bplot()
